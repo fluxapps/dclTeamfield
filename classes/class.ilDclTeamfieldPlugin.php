26f82a0e0deb58cabbd3a7cf5160d484ad3f6f27
Nicolas Schäfli <nicolas.schaefli@gmail.com>
2016-10-18 09:47:48 +0200

    DI aware plugin class created

---
+++ classes/class.ilDclTeamfieldPlugin.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Class ilDclTeamfieldPlugin
+ *
+ * @author  Nicolas Schäfli <ns@studer-raimann.ch>
+ */
+
+namespace DclTeamfield;
+
+require_once('./Modules/DataCollection/classes/Fields/Plugin/class.ilDclFieldTypePlugin.php');
+
+class ilDclTeamfieldPlugin extends \ilDclFieldTypePlugin
+{
+    const PLUGIN_NAME = "DclTeamfield";
+
+    /**
+     * ilDclTeamfieldPlugin constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        if(!$GLOBALS["DIC"]->offsetExists(self::class))
+        {
+            $GLOBALS["DIC"][self::class] = $this;
+        }
+    }
+
+    /**
+     * Provides a new or already created ilDclTeamfieldPlugin instance.
+     *
+     * @return ilDclTeamfieldPlugin
+     */
+    public static function getInstance()
+    {
+        if(!$GLOBALS["DIC"]->offsetExists(self::class))
+        {
+            return new self();
+        }
+
+        return $GLOBALS["DIC"][self::class];
+    }
+
+    function getPluginName()
+    {
+        return self::PLUGIN_NAME;
+    }
+
+}
